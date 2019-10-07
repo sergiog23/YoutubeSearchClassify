f7219188e9031c8df9e94e455940c5f2b8944287
Sergio Guerrero <sergiog@wingstop.com>
2019-10-07 13:56:16 -0500

    Update app.py

--- TestAPP/app.py
+++ TestAPP/app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def index():
     return render_template('home.html')
 
+@app.route('/search')
+def search():
+    return render_template('search.html')
+
 if (__name__ == '__main__'):
     app.run(debug=True)
