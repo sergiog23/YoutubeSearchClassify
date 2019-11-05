f8dd3f1e8e3c700ca3538f2436a773e45ff20658
Sergio Guerrero <34423081+sergiog23@users.noreply.github.com>
2019-11-05 17:08:57 -0600

    Update UpdateSearchEngine.py

--- TestAPP/UpdateSearchEngine.py
+++ TestAPP/UpdateSearchEngine.py
@@ -31,8 +31,21 @@ for i in range(100):
     curDoc = allData.iloc[i]['title']
     curDoc = curDoc+ allData.iloc[i]['description']
     tokenized = regExToken.tokenize(curDoc)
+        tokenized = tokenized.tolower()
+    for token in tokenized:
+        if token not in dictionary:
+            dictionary.append(token)
+for document in tokenized:
+    weight_vector = {}
+
+    for token in document:
+        if token not in weight_vector:
+            TF = document.count(token)/len(document)
+            DF = sum (1 for document in processedDoc if token in document)
     
     
+    
+
     
 
 
