cd9c0791ce24eee7cc9e60441868668f2daa1e98
Sergio Guerrero <sergiog0698@gmail.com>
2019-11-27 12:25:31 -0600

    added classifier to data

--- AppDataMining/app.py
+++ AppDataMining/app.py
@@ -1,16 +1,18 @@
 from flask import Flask, render_template
 from flask import request
-from Search  import *
-from SearchApps import * 
+from SearchApps  import *
+from classifier import *
+
 
 app = Flask(__name__)
 
 result = []
 test = AppSearch()
+classs = AppClassifer()
 
 @app.route('/')
 def index():
-    return render_template('home.html', data=yes.vidInfo, result=result, search=False)
+    return render_template('home.html', data=test.vidInfo, result=result, search=False)
 
 
 @app.route('/', methods=['POST'])
@@ -31,13 +33,25 @@ def search():
     idf = []
 
     for i in documents:
-        tfs = yes.tfi[i]
+        tfs = test.tfi[i]
         tf.append(tfs)
     for i in documents:
-        ids = yes.tf_IDF[i]
+        ids = test.tf_IDF[i]
+
         idf.append(ids)
 
-    return render_template('results.html', data=yes.vidInfo,userQuery=userQuery, result=documents, title=title, description=description,l=l,tf=tf,idf=idf, search=True)
+    return render_template('results.html', data=test.vidInfo,userQuery=userQuery, result=documents, title=title, description=description,l=l,tf=tf,idf=idf, search=True)
+@app.route('/', methods=['POST'])
+def classify():
+    userQuery = request.form.get('classify')
+    categories = []
+    percentage = []
+    classification = classs.classify(classify) 
+    for c in classification:
+        categories.append(c)
+        percentage.append(round(classification[c]*100,2))
+    return render_template('resultsClass.html',data=classs.vidInfo,userQuery=userQuery,categories=categories,percentage=percentage,classification=classification,search=False)
+
 
 
 if (__name__ == '__main__'):
