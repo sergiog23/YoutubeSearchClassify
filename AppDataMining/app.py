e04daa1f70cd536c9868e8f2ebee838a8c5283ff
Sergio Guerrero <sergiog@wingstop.com>
2019-12-02 09:33:34 -0600

    Update app.py

--- AppDataMining/app.py
+++ AppDataMining/app.py
@@ -1,18 +1,18 @@
 from flask import Flask, render_template
 from flask import request
-from SearchApps  import *
+from search  import *
 from classifier import *
 
 
 app = Flask(__name__)
 
 result = []
-test = AppSearch()
-classs = AppClassifer()
+searchObj = AppSearch()
+classifyObj = AppClassifer()
 
 @app.route('/')
 def index():
-    return render_template('home.html',data=test.vidInfo,result=result, search=False,classify=False)
+    return render_template('home.html',data=searchObj.vidInfo,result=result, search=False,classify=False)
 
 
 @app.route('/', methods=['POST'])
@@ -21,22 +21,22 @@ def Compute():
     searchQuery = request.form.get('Search')
     if(searchQuery!= None):
 
-        result = test.search(searchQuery)
+        result = searchObj.search(searchQuery)
         l = len(result)
         for i in range(l):
             documents.append(result[i])
-        title = list(test.vidInfo['track_name'][documents])
-        description = list(test.vidInfo['app_desc'][documents])
+        title = list(searchObj.vidInfo['track_name'][documents])
+        description = list(searchObj.vidInfo['app_desc'][documents])
         tf =[]
         idf = []
 
         for i in documents:
-            tfs = test.tfi[i]
+            tfs = searchObj.tfi[i]
             tf.append(tfs)
         for i in documents:
-            ids = test.tf_IDF[i]
+            ids = searchObj.tf_IDF[i]
             idf.append(ids)
-        return render_template('results.html', data=test.vidInfo,userQuery=searchQuery, result=documents, title=title, description=description,l=l,tf=tf,idf=idf, search=True,classify = False)
+        return render_template('results.html', data=searchObj.vidInfo,userQuery=searchQuery, result=documents, title=title, description=description,l=l,tf=tf,idf=idf, search=True,classify = False)
 
     classifyQuery = request.form.get('Classify')
     if(classifyQuery != None):
@@ -44,14 +44,14 @@ def Compute():
         categories = []
         percentage = []
         priors = []
-        classification = classs.classify(classifyQuery) 
-        testing = classs.counts
-        priors.append(testing)
+        classification = classifyObj.classify(classifyQuery) 
+        searchObjing = classifyObj.counts
+        priors.append(searchObjing)
         for c in classification:
             categories.append(c)
             percentage.append(round((classification[c]*100),2))
             
-        return render_template('resultsClass.html',data=classs.vidInfo,classifyQuery=classifyQuery,categories=categories,percentage=percentage,classification=classification,search=False,classify=True,priors=priors)
+        return render_template('resultsClass.html',data=classifyObj.vidInfo,classifyQuery=classifyQuery,categories=categories,percentage=percentage,classification=classification,search=False,classify=True,priors=priors)
 
 
 
